bc4fa8cba338d5e6ee33a47cf39af46a865e7a95
nardtree <gim.kobayashi@gmail.com>
2017-09-30 22:17:05 +0900

    update

--- adult_dataset/some.py
+++ adult_dataset/some.py
@@ -13,18 +13,21 @@ X = np.array(X)
 y = np.array(y)
 
 if '--grid_search' in sys.argv:
-  parameters = {
-    'n_estimators'      : [5, 10, 20, 30, 50, 100, 300],
-    'max_features'      : [3, 5, 10, 15, 20],
-    'random_state'      : [0],
-    'n_jobs'            : [1],
-    'min_samples_split' : [3, 5, 10, 15, 20, 25, 30, 40, 50, 100],
-    'max_depth'         : [3, 5, 10, 15, 20, 25, 30, 40, 50, 100]
-  }
-  clf = grid_search.GridSearchCV(RandomForestClassifier(), parameters)
-  clf.fit(X, y)
-
-  print( clf.best_estimator_)
+  if '--fit' in sys.argv:
+    parameters = {
+      #'n_estimators'      : [5, 10, 20, 30, 50, 100, 300],
+      'max_features'      : [3, 5, 10, 15, 20, 25],
+      'min_samples_split' : [3, 5, 10, 15, 20, 25, 30, 40, 50, 100],
+      'min_samples_leaf'  : [1, 2, 3, 5, 10, 15, 20],
+      'max_depth' : [10, 20, 25, 30]
+    }
+    clf = grid_search.GridSearchCV(RandomForestClassifier(), parameters, n_jobs=16)
+    clf.fit(X, y)
+    open( 'clf.pkl', 'wb').write( pickle.dumps( clf ) )
+  clf = pickle.loads( open( 'clf.pkl','rb' ).read() )
+  print('best_estimator',  clf.best_estimator_ )
+  print('best param', clf.best_params_ )
+  print('best score',  clf.best_score_ )
 
 
 #clf = RandomForestClassifier(max_depth=2, random_state=0)
